315a34a9f70881a4a68bf49671e21ad9133408a3
Jihan Park <parkjihan5253@gmail.com>
2022-08-19 03:47:35 +0900

    style
     style

--- FRONTEND/src/routes/Home.js
+++ FRONTEND/src/routes/Home.js
@@ -1,4 +1,4 @@
-import JweetBox from "components/box/JweetBox";
+import YONBoxSlider from "components/box/YONBoxSlider";
 import LoadingBox from "components/box/LoadingBox";
 import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
 import { db } from "mybase";
@@ -35,34 +35,28 @@ const Home = () => {
 	}, []);
 	return (
 		<div class="flex flex-row w-full justify-center">
-			<div class="flex flex-col pt-16 max-w-4xl">
-
-			<Swiper
-            // install Swiper modules
-            slidesPerView="1"
-            mousewheel={true}
-            direction="vertical"
-            modules={[Pagination]}
-            pagination={{ clickable: true }}
-            scrollbar={{ draggable: true }}
-        >
-      
-
-				<div class="pb-20">
-					{jweets.length !== 0 ? (
-						jweets.map((jweet, index) => {
-							return <SwiperSlide><JweetBox key={jweet.id} jweet={jweet} id={jweet.id} /></SwiperSlide>;
-						})
-					) : loading ? (
-						<div class="w-full flex-1 flex justify-center items-center mt-8">
-							등록된 Jweet이 없습니다.
-						</div>
-					) : (
-						<LoadingBox />
-					)}
-				</div>
+			<div class="flex flex-col pt-16 pb-20 max-w-4xl">
+				<Swiper
+					slidesPerView="1"
+					mousewheel={true}
+					direction="vertical"
+					modules={[Pagination]}
+					pagination={{ clickable: true }}
+				>
+					<div>
+						{jweets.length !== 0 ? (
+							jweets.map((jweet, index) => {
+								return <SwiperSlide><YONBoxSlider key={jweet.id} jweet={jweet} id={jweet.id} /></SwiperSlide>;
+							})
+						) : loading ? (
+							<div class="w-full flex-1 flex justify-center items-center mt-8">
+								등록된 Jweet이 없습니다.
+							</div>
+						) : (
+							<LoadingBox />
+						)}
+					</div>
 				</Swiper>
-
 			</div>
 		</div>
 	);
