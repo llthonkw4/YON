91a01411b0eca2d1ba7f73100164319728c72659
robert lee <minsu.rob@gmail.com>
2022-08-20 02:44:10 +0900

    깃허브 회원가입 삭제

--- FRONTEND/src/components/container/RegisterContainer.js
+++ FRONTEND/src/components/container/RegisterContainer.js
@@ -10,7 +10,6 @@ const RegisterContainer = ({ handleOpen, toggleLogin }) => {
 				오늘 Jwitter에 가입하세요.
 			</h1>
 			<GoogleButton isLogin={false} />
-			<GithubButton isLogin={false} />
 			<EmailButton handleOpen={handleOpen} isLogin={false} />
 			<h1 class="w-full md:w-1/2 mb-4">
 				By signing up, you agree to the{" "}
