043cb9ceb30da95bdd635f357ea9feb86c7031d6
178kg78cm <jhh7550@gmail.com>
2022-08-19 20:14:07 +0900

    Bookmark : change to Like
    Change Bookmark to Like

--- FRONTEND/src/routes/Bookmark.js
+++ FRONTEND/src/routes/Bookmark.js
@@ -75,7 +75,7 @@ const Bookmark = () => {
 					) : loading ? (
 						<div class="w-full flex flex-col justify-center items-center mt-8">
 							<div class="w-2/3 font-bold text-2xl">
-								You haven’t added any Jweets to your Bookmarks yet
+								You haven’t added any YoNs to your Likes yet
 							</div>
 							<div class="w-2/3 text-gray-500">
 								When you do, they’ll show up here.
