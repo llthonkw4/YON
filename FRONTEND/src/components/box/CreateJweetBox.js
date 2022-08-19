5b146a614de9d2e92f8528e735d28c7da3a10507
arthurjo <iii1003@naver.com>
2022-08-20 00:00:35 +0900

    style : Jweet 수정 및 색 변경

--- FRONTEND/src/components/box/CreateJweetBox.js
+++ FRONTEND/src/components/box/CreateJweetBox.js
@@ -218,8 +218,8 @@ const CreateJweetBox = ({ isModal, discardThread }) => {
 					<div>
 						<input
 							type="submit"
-							class="text-sm w-auto rounded-full text-white font-bold bg-purple-400 flex justify-center px-4 py-2 hover:bg-purple-600 transition delay-50 duration-300 cursor-pointer"
-							value="Jweet"
+							class="text-sm w-auto rounded-full text-white font-bold bg-black flex justify-center px-4 py-2 hover:bg-purple-600 transition delay-50 duration-300 cursor-pointer"
+							value="Post"
 						/>
 					</div>
 				</div>
